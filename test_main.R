ead076234250c96903a437941dec9f152a73c272
adamlabadorf <adamlabadorf@bf591-admin-tools.deploy-assignment>
2023-10-12 21:22:20 +0000

    See https://github.com/BF591-R/test-assignment-source/commit/63c2f29a06b33a4c74f30c4bd87496dd3f906370 from refs/heads/main

--- test_main.R
+++ test_main.R
@@ -2,6 +2,14 @@ library(testthat)
 
 source('main.R')
 
-test_that("metatest", {
-  expect_equal(add3(3), 6)
+describe("add3", {
+  it("adds 3 to 3 and gets 6", {
+      expect_equal(add3(3), 6)
+  })
+  it("add 3 to a vector of numbers", {
+     expect_equal(add3(c(1,2,3),c(4,5,6)))
+  })
+  it("fails when given invalid input", {
+     expect_error(add3("3"))
+  })
 })
