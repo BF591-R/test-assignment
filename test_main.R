1438803711d328be69e53075322f4e4f0ec337c4
adamlabadorf <adamlabadorf@bf591-admin-tools.deploy-assignment>
2023-10-12 21:46:27 +0000

    See https://github.com/BF591-R/test-assignment-source/commit/955186c8255daab9a2d35c9cd286e71483887457 from refs/heads/main

--- test_main.R
+++ test_main.R
@@ -7,7 +7,7 @@ describe("add3", {
       expect_equal(add3(3), 6)
   })
   it("add 3 to a vector of numbers", {
-     expect_equal(add3(c(1,2,3),c(4,5,6)))
+     expect_equal(add3(c(1,2,3)),c(4,5,6))
   })
   it("fails when given invalid input", {
      expect_error(add3("3"))
